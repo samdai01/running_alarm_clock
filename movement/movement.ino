c1ed048e51fe62cefa52cf0165a58847a7063476
Sam <daisam1215@gmail.com>
2023-10-15 15:59:46 -0700

    Documentation on movement ino
    Added Basic C++ docs on the arduino code

--- movement/movement.ino
+++ movement/movement.ino
@@ -1,15 +1,29 @@
+/**
+ * @file movement.ino
+ * @author Sam Dai (daisam1215@gmail.com)
+ * @brief Main test arduino sketck to test movement of the alarm clock buggy
+ * @date 2023-10-04
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
 #include <avr/wdt.h>
 #include "motor.h"
 
 motor alarm_clock_motor;
 
+/**
+ * @brief Arduino setup function.
+ */
 void setup() {
-    alarm_clock_motor.motorInit();
-    delay(2000);
-    alarm_clock_motor.forward();
-    delay(2000);
+    alarm_clock_motor.motorInit();      // initializing motor pins
 }
 
+/**
+ * @brief Arduino loop function. 
+ * NOTE: This function will endlessly loop unless arduino is turned off
+ */
 void loop() {
     alarm_clock_motor.stop();
     delay(2000);
